f3cd4fae94d37e7812ab8145db5e7b5d0fed45cc
HeJunCheng <he_jun_cheng@163.com>
2020-03-21 21:52:44 +0800

    项目构建、配置全局样式，添加element-ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="login">
+    <el-button type="success">成功按钮</el-button>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+  #login {
+    height: 100vh;
+    background-color: #344a5f;
+  }
+</style>
